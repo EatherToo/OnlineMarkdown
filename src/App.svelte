5f1ee72ec4005333d4bb0c9c439a6e9b2dcd11c8
Eather <eathertoo@gmail.com>
2022-08-28 09:18:54 +0800

    feat: add markdown render

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
+  import MarkdownIt from 'markdown-it'
+
+  const md = new MarkdownIt({
+    html: false,
+    xhtmlOut: false,
+    breaks: true,
+    langPrefix: 'language-',
+
+    linkify: true,
+
+    typographer: false,
+
+    quotes: '“”‘’',
+
+    highlight: function (/*str, lang*/) {
+      return ''
+    },
+  })
+  let text = ''
+  $: renderRes = md.render(text)
 </script>
 
 <main>
-  <div class="markdown-wrapper w-100">
-    <div class="markdown-editor" />
-    <div class="markdown-viewer" />
+  <input
+    placeholder="请输入标题"
+    class="h-[54px] border-b-solid border-b-[1px] border-b-[#d6d8dc] outline-none w-full text-[24px] font-bold pl-[25px] py-[15px]"
+  />
+
+  <div class="markdown-wrapper w-full h-[100vh] flex">
+    <div
+      class="markdown-editor w-[50%] border-r-solid border-r-[1px] border-r-[#d6d8dc] h-full bg-[#f8f9fa]"
+    >
+      <textarea
+        on:keypress={(e) => {
+          console.log(e)
+        }}
+        bind:value={text}
+        class="w-full h-full bg-[#f8f9fa] p-[20px] outline-none"
+      />
+    </div>
+    <div class="markdown-viewer w-[50%] h-full p-[20px] bg-[#ffffff]">
+      {@html renderRes}
+    </div>
   </div>
 </main>
 
